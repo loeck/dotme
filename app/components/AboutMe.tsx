9f108c9558a661605a248b0fb3597516926aed0d
Alex Locatelli <alex@join-jump.com>
2023-06-14 14:14:21 +0200

    update company name and job position

--- app/components/AboutMe.tsx
+++ app/components/AboutMe.tsx
@@ -26,11 +26,11 @@ export const AboutMe = ({ variant }) => (
       <div>
         I work in Paris at{' '}
         <Highlight variant={variant}>
-          <a href="https://www.join-jump.com/" target="_blank" rel="noreferrer">
-            Jump
+          <a href="https://www.regate.io/" target="_blank" rel="noreferrer">
+            Regate
           </a>
         </Highlight>{' '}
-        as a Lead Frontend Developer
+        as a Lead Frontend Architect
       </div>
       <div className="flex gap-2">
         <Highlight variant={variant}>
@@ -49,7 +49,6 @@ export const AboutMe = ({ variant }) => (
         </Highlight>
         <Highlight variant={variant}>
           <a
-            href="https://www.join-jump.com/"
             target="_blank"
             rel="noreferrer"
             href="https://last.fm/user/NainPuissant"
